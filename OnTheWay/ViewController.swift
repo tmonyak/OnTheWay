cba6e5c8fbc2398b8ea4262f2f6336c51b8a5292
Ted Monyak <monyak@usc.edu>
2016-06-21 22:31:06 -0700

    InitialCommit

--- OnTheWay/ViewController.swift
+++ OnTheWay/ViewController.swift
@@ -8,18 +8,39 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    //MARK: Properties
+    
+    @IBOutlet weak var originTextField: UITextField!
+    @IBOutlet weak var originLabel: UILabel!
+    @IBOutlet weak var destinationLabel: UILabel!
+    @IBOutlet weak var destinationTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        originTextField.delegate = self;
+        destinationTextField.delegate = self;
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    //MARK: Actions
+    
+    @IBAction func findRestaurants(sender: UIButton) {
+    }
 
 }
 
