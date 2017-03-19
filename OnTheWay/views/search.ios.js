d5c5e0842e00c961f35744f66f3c1b3b842a48c1
Ted Monyak <monyak@usc.edu>
2017-03-19 15:56:39 -0700

    working

--- OnTheWay/views/search.ios.js
+++ OnTheWay/views/search.ios.js
@@ -16,16 +16,32 @@ import OAuthSimple from 'oauthsimple';
 
 import DefaultTheme from './../themes/theme';
 
-var googleAPIUrl = 'https://maps.googleapis.com/maps/api/directions/json?';
+var googleAPIURL = 'https://maps.googleapis.com/maps/api/directions/json?';
 var googleAPIKey = 'AIzaSyAfDs-3kqizJ3lMrCsZ5dYZpsAOOZz8dkA';
 
-var yelpAPIUrl = 'https://api.yelp.com/v2/search';
+var yelpAPIURL = 'https://api.yelp.com/v2/search';
 var yelpConsumerKey	= 'nkiyebEprDAa_GC6TT5L5g';
 var yelpConsumerSecret = 'XqYls7BGA_GJOKn6I3900CTB1ko';
 var yelpToken = '-p9bLMHtgE9a5ARyW6wlDJuejD6egddU';
 var yelpTokenSecret = 'eyjhyDOhKPbpqi7tKuv_ZnmuSwA';
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+
+var styles = StyleSheet.create({
+    list: {
+        justifyContent: 'center',
+        flexDirection: 'row',
+        flexWrap: 'wrap'
+    },
+
+    item: {
+        margin: 5,
+        width: 170,
+        height: 170,
+      alignItems:'center'
+    }
+
+});
 
 class Search extends Component {
     constructor(props) {
@@ -34,7 +50,8 @@ class Search extends Component {
             latitude: null,
             longitude: null,
             results: null,
-            useCurrentLocation: false
+            useCurrentLocation: false,
+            dataSource: ds.cloneWithRows([])
         }
     }
 
@@ -58,25 +75,29 @@ class Search extends Component {
             origin = 'Los Angeles,CA';
         }
         var destination = 'San Diego,CA';
-        var googleAPI = googleAPIUrl + 'origin=' + origin + '&destination=' + destination + '&key=' + googleAPIKey;
+        var googleAPI = googleAPIURL + 'origin=' + origin + '&destination=' + destination + '&key=' + googleAPIKey;
 
         var oauth = new OAuthSimple(yelpConsumerKey, yelpTokenSecret);
-        var yelpRequest = oauth.sign({
-            action: "GET",
-            path: yelpAPIUrl,
-            parameters: "term=coffee&" + 'll=' + origin,
-            signatures: {api_key: yelpConsumerKey, shared_secret: yelpConsumerSecret, access_token: yelpToken,
-            access_secret: yelpTokenSecret},
 
-        });
         fetch(googleAPI)
         .then((response) => response.json())
         .then((result) => {
+            alert(result)
+            alert(googleAPI)
+            this.setState({
+                dataSource: ds.cloneWithRows(result.routes),
+            })
+            /*var yelpRequest = oauth.sign({
+                action: "GET",
+                path: yelpAPIURL,
+                parameters: "term=coffee&" + 'll=' + origin,
+                signatures: {api_key: yelpConsumerKey, shared_secret: yelpConsumerSecret, access_token: yelpToken,
+                access_secret: yelpTokenSecret},
+            });
             fetch(yelpRequest.signed_url, {method: "GET"})
             .then((response) => response.json())
             .then((result) => {
-                alert("success");
-            });
+            });*/
         });
     }
 
@@ -100,9 +121,20 @@ class Search extends Component {
            style={{backgroundColor: '#5d0dbf', marginRight: 10, marginLeft: 10}}>
                <Text style={{color: 'white', fontSize: 18}}> Find Stuff </Text>
            </Button>
-           <Text>
-               {this.state.results}
-           </Text>
+           <View>
+               <ListView
+                dataSource={this.state.dataSource}
+                enableEmptySections={true}
+                renderRow={(rowData) =>  <View style={styles.item}>
+               <View>
+               <Text>
+                 {rowData.name}
+                </Text>
+               </View>
+                </View>}
+                />
+           </View>
+
            </Content>
        </Container>
         );
